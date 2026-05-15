97481d197c5b10da31673907ba322d698de837df
Markiyan Baziuk <markiyanbaziuk@knu.ua>
2026-05-15 10:47:00 +0300

    Добавив main

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,6 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe, Category
 
+def main(request):
+    recipes = Recipe.objects.order_by('?')[:10]
+    return render(request, 'main.html', {'recipes': recipes})
