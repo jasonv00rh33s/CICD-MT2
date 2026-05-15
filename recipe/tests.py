1e0d3d80a3beff9ed22613a12c522255beb813c6
Markiyan Baziuk <markiyanbaziuk@knu.ua>
2026-05-15 10:52:55 +0300

    Створив тест для перевірки правильності рецепту

--- recipe/tests.py
+++ recipe/tests.py
@@ -13,3 +13,8 @@ class RecipeViewTests(TestCase):
 def test_main_view_status_code(self):
         response = self.client.get(reverse('main'))
         self.assertEqual(response.status_code, 200)
+
+def test_category_detail_view_status_code(self):
+        response = self.client.get(reverse('category_detail', args=[self.category.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Cake")
